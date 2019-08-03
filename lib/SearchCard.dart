202fab36950b5da8f08a0f263d19d9f338503666
danyiliev <danyiliev5@gmail.com>
2019-08-03 15:00:45 -0400

    hide keyboard ontap

--- lib/SearchCard.dart
+++ lib/SearchCard.dart
@@ -2,32 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:wheelio/Logo.dart';
 import 'package:wheelio/SearchForm.dart';
 
-class SearchCard extends StatelessWidget {
+class SearchCard extends StatefulWidget {
+  @override
+  _SearchCardState createState() => _SearchCardState();
+}
+
+class _SearchCardState extends State<SearchCard> {
+  var showFlag = true;
+  var length = 127.0;
+
+  void _hide() {
+    setState(() {
+      length = 25.0;
+      showFlag = false;
+    });
+  }
+
+  void _show() {
+    setState(() {
+      length = 127.0;
+      showFlag = true;
+    });
+  }
+
   final BoxDecoration decoration = new BoxDecoration(
     image: DecorationImage(
       image: AssetImage("background2.png"),
       fit: BoxFit.cover,
     ),
   );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
-      body: Container(
-        decoration: decoration,
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            color: Colors.black.withOpacity(0.1),
-            child: Column(
-              children: <Widget>[
-                Logo(),
-                SizedBox(
-                  height: 59,
-                ),
-                SearchForm(),
-              ],
+      body: GestureDetector(
+        onTap: () {
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+              _show();
+            }
+        },
+        child: Container(
+          decoration: decoration,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black.withOpacity(0.1),
+              child: Column(
+                children: <Widget>[
+                  Logo(),
+                  SizedBox(
+                    height: 59,
+                  ),
+                  SearchForm(
+                    length: length,
+                    showFlag: showFlag,
+                    show: _show,
+                    hide: _hide,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/SearchForm.dart
+++ lib/SearchForm.dart
@@ -1,106 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:wheelio/SearchField.dart';
+import 'package:wheelio/StyledButton.dart';
 
-class SearchForm extends StatefulWidget {
+class SearchForm extends StatelessWidget {
+  SearchForm({Key key, this.length, this.showFlag, this.show, this.hide}) : super(key: key);
+  final double length;
+  final bool showFlag;
+  final Function show;
+  final Function hide;
+
   @override
-  _SearchFromState createState() => _SearchFromState();
-}
-
-class _SearchFromState extends State<SearchForm> {
-  var showFlag = true;
-  var length = 127.0;
-
-  void _hide() {
-    setState(() {
-      length = 25.0;
-      showFlag = false;
-    });
-  }
-
-  void _show() {
-    setState(() {
-      length = 127.0;
-      showFlag = true;
-    });
-  }
-
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        GestureDetector(
+        SearchField(
+          label: 'Find your next amazing vehicle!',
           onTap: () {
-            FocusScopeNode currentFocus = FocusScope.of(context);
-
-            if (!currentFocus.hasPrimaryFocus) {
-              currentFocus.unfocus();
-              _hide();
-              print('ddd');
-            }
+            hide();
           },
-          child: Column(
-            children: <Widget>[
-              SearchField(
-                label: 'Find your next amazing vehicle!',
-                onTap: () {
-                  _hide();
-                },
-                onSubmitted: (v) {
-                  _show();
-                },
-              ),
-              SizedBox(height: length),
-              StyledButton(
-                title: 'SEARCH',
-                color: Color(0xFF333333),
-                fontColor: Colors.white,
-                onPressed: () {
-                  Navigator.pushNamed(context, '/googlemap');
-                },
-              ),
-              if (showFlag == true) ...[
-                SizedBox(height: 33),
-                Text('OR', style: TextStyle(color: Color(0xFF333333))),
-                SizedBox(height: 33),
-                StyledButton(
-                  title: 'POST A CAR FOR SALE',
-                  color: null,
-                  onPressed: null,
-                  fontColor: Color(0xFF333333),
-                ),
-              ],
-            ],
-          ),
+          onSubmitted: (v) {
+            show();
+          },
         ),
+        SizedBox(height: length),
+        StyledButton(
+          title: 'SEARCH',
+          color: Color(0xFF333333),
+          fontColor: Colors.white,
+          onPressed: () {
+            Navigator.pushNamed(context, '/googlemap');
+          },
+        ),
+        if (showFlag == true) ...[
+          SizedBox(height: 33),
+          Text('OR', style: TextStyle(color: Color(0xFF333333))),
+          SizedBox(height: 33),
+          StyledButton(
+            title: 'POST A CAR FOR SALE',
+            color: null,
+            onPressed: null,
+            fontColor: Color(0xFF333333),
+          ),
+        ],
       ],
-    );
-  }
-}
-
-class StyledButton extends StatelessWidget {
-  StyledButton(
-      {Key key, this.title, this.color, this.fontColor, this.onPressed})
-      : super(key: key);
-
-  final String title;
-  final Color color;
-  final Color fontColor;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: 300,
-      child: FlatButton(
-        child: Text(title,
-            style: TextStyle(
-                color: fontColor, fontSize: 14, fontWeight: FontWeight.bold)),
-        color: color,
-        onPressed: onPressed,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-            side: BorderSide(color: Color(0xFF333333), width: 1)),
-      ),
     );
   }
 }
