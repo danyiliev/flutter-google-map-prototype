1aa23eb752964341cbb4afbecf15524302ea9319
danyiliev <danyiliev5@gmail.com>
2019-08-04 12:30:45 -0400

    add fade effect to the splash screen navigation

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:wheelio/LogoCard.dart';
+import 'package:wheelio/SearchCard.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,9 +10,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   void navigateToSearch() {
-    Navigator.pushNamed(context, '/search');
+    Navigator.push(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (c, a1, a2) => SearchCard(),
+          transitionsBuilder: (c, anim, a2, child) =>
+              FadeTransition(opacity: anim, child: child),
+          transitionDuration: Duration(milliseconds: 2000),
+        ));
+    //Navigator.pushNamed(context, '/search');
   }
 
   startTime() async {
